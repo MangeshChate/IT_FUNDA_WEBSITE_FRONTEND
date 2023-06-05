a986986d973e766f7ea1453a9576c4e1fd45730d
mangesh <mbchate08@gmail.com>
2023-06-05 23:45:00 +0530

    gf

--- src/Components/LandingPage/Navbar.js
+++ src/Components/LandingPage/Navbar.js
@@ -3,23 +3,27 @@ import React from 'react'
 export default function Navbar() {
   return (
    
-         <header>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
-            <div className="container">
-                <a className="navbar-brand fw-bold" href="#">OMPS</a>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link btn btn-success fw-bold" href="index2.php">Login</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link btn btn-success fw-bold ms-2" href="http://itfunda.rf.gd/signup.html">Register</a>
-                        </li>
-                    </ul>
-                </div>
+    <header>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+        <div className="container">
+            <a className="navbar-brand fw-bold" href="#">OMPS</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link btn btn-success fw-bold" href="index2.php">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link btn btn-success fw-bold ms-2" href="http://itfunda.rf.gd/signup.html">Register</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    </header>
+        </div>
+    </nav>
+</header>
+
     
   )
 }
